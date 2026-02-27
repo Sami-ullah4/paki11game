2c5ad7f89f07f82f82e0b72a13147183a25f3872
Sami-ullah4 <samiullah0313066@gmail.com>
2026-02-28 00:37:06 +0500

    sdsads

--- app/layout.js
+++ app/layout.js
@@ -63,6 +63,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <meta name="google-site-verification" content="V_Q90g7Yd3Y36pM57okef2s3JkgPXIy9asl7XRZYJCE" />
         {/* Organization Schema */}
         <script
           type="application/ld+json"
